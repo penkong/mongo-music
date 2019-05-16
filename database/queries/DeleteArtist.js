4f40847e1da74baf8bd010739725343517186e9b
mazdak <nazemi.works@gmail.com>
2019-05-16 02:28:11 -0700

    crud in mongodb

--- database/queries/DeleteArtist.js
+++ database/queries/DeleteArtist.js
@@ -5,5 +5,8 @@ const Artist = require('../models/artist');
  * @param {string} _id - The ID of the artist to delete.
  * @return {promise} A promise that resolves when the record is deleted
  */
-module.exports = (_id) => {
-};
+module.exports = (_id) => Artist.remove({ _id });
+// module.exports = (_id) => {
+//   return Artist.remove({ _id });
+// };
+
