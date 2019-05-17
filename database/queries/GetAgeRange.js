a28a421cb2beb754e83d0511d6e1f47190f854ac
mazdak <nazemi.works@gmail.com>
2019-05-16 23:16:00 -0700

    query for min and max from db

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,21 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const minQuery = Artist
+    .find({})
+    .sort({ age: 1 })
+    .limit(1) //after this is still bring {[]} must catch age from there.
+    .then(artists => artists[0].age); //limit api for next time just bring us age
+
+  const maxQuery = Artist
+    .find({})
+    .sort({ age: -1 })
+    .limit(1) //after this is still bring {[]} must catch age from there.
+    .then(artists => artists[0].age); //limit api for next time just bring us age
+
+  return Promise.all([minQuery, maxQuery])
+    .then(result => {
+      return { min: result[0], max: result[1] };
+    });
 };
+//GetAgeRange().then(args => cl(args));
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minQuery = Artist
+    .find({})
+    .sort({ yearsActive: 1 })
+    .limit(1) //after this is still bring {[]} must catch yearsActive from there.
+    .then(artists => artists[0].yearsActive); //limit api for next time just bring us yearsActive
+
+  const maxQuery = Artist
+    .find({})
+    .sort({ yearsActive: -1 })
+    .limit(1) //after this is still bring {[]} must catch yearsActive from there.
+    .then(artists => artists[0].yearsActive); //limit api for next time just bring us age
+
+  return Promise.all([minQuery, maxQuery])
+    .then(result => {
+      return { min: result[0], max: result[1] };
+    });
 };
