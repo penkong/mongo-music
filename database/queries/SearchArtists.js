6eec2487764dda922aa99eaf38c4705986668591
mazdak <nazemi.works@gmail.com>
2019-05-17 01:21:30 -0700

    search for porps with many specifity with pagination check it

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,34 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = Artist.find(buildQuery(criteria))
+    .sort({ [sortProperty]: 1 })//es6 interpolate key
+    .skip(offset)
+    .limit(limit);
+  return Promise.all([query, Artist.count()])
+    .then(results => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset,
+        limit
+      };
+    });
+};
+
+const buildQuery = (criteria) => {
+  const query = {};
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+  return query;
 };
